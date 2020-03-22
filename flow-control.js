f2b3af6e16c9a17cc3658466f08430415f4a4bf7
Ben Anderson <andersjbe@gmail.com>
2020-03-22 21:12:47 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -20,7 +20,7 @@ function ageChecker(age) {
   } else if(age >= 13 && age <= 19) {
     return "You are a teenager!";
   } else {
-    return "You are an adult";
+    return "You are a grownup";
   }
 }
 
